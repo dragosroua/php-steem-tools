a97283c147d3ce62bea4f2e96632effd6ffcb4f6
Dragos Roua <dragosroua@gmail.com>
2017-12-17 13:18:43 -0500

    clean up example

--- examples/authorPayments.php
+++ examples/authorPayments.php
@@ -2,11 +2,10 @@
 	
 require __DIR__ . '/vendor/autoload.php';
 
-	$config['webservice_url'] = 'gtg.steem.house:8090';
+	$config['webservice_url'] = 'steemd.minnowsupportproject.org';
 	$api = new DragosRoua\PHPSteemTools\SteemApi($config);
 	
-	$author = "stellabelle";
-	
+	$author = "utopian-io";
 	
 	date_default_timezone_set('UTC');
 	$dateNow = (new \DateTime())->format('Y-m-d\TH:i:s'); 
@@ -14,16 +13,15 @@ require __DIR__ . '/vendor/autoload.php';
 	$params = [$author, '', $date_8days_ago, 100];
 	
 	$remote_content = $api->getDiscussionsByAuthorBeforeDate($params, 'websocket');	
-	print_r($remote_content);
+	//print_r($remote_content);
 	
 	if(isset($remote_content)){
 		if (array_key_exists('error', $remote_content)){
-			$return_value = "<br/>Something is not ok.</br>";
+			echo "Something is not ok. You should investigate more.\n";
 		}
 		else 
 		{
 			$total_value = 0;
-			$total_votes = 0;
 			
 			foreach($remote_content as $rkey => $rvalue){
 				if($rvalue['pending_payout_value'] !== '0.000 SBD' && $rvalue['max_accepted_payout'] != 0){
@@ -39,17 +37,13 @@ require __DIR__ . '/vendor/autoload.php';
 							}
 						}
 					}
-					$total_value =+ $payout * 0.75;
+					$author_payout = $payout * 0.75;
+					echo "Author payout: ".$author_payout.", Total post payout: ".$payout."\n";
+					$total_value += $author_payout;
 				}
 			}
-			echo "Total payout for $author: ".$total_value."\n";
+			echo "Total payout for $author: ".$total_value." (SBD + STEEM)\n";
 		}
 	}
-
-	
-
-
-
-
 
 ?>
